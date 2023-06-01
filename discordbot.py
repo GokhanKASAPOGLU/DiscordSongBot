ee7d75a2d8f9f4e5c8df64ae1ea09831f93a312d
Gökhan KASAPOĞLU <135224314+GokhanKASAPOGLU@users.noreply.github.com>
2023-06-01 15:01:47 +0300

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -9,7 +9,7 @@ intents = discord.Intents.default()
 intents.voice_states = True
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='', intents=intents)
+bot = commands.Bot(command_prefix='-', intents=intents)
 
 @bot.event
 async def on_ready():
